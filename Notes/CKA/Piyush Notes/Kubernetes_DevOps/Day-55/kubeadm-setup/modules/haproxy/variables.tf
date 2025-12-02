c1c74cfc9f2e9f9b08dce194407770087af4cb7a
Juhi Sinha <46685880+juhisinha422@users.noreply.github.com>
2025-12-02 19:34:46 +0530

    Create variables.tf

---
+++ Notes/CKA/Piyush Notes/Kubernetes_DevOps/Day-55/kubeadm-setup/modules/haproxy/variables.tf
@@ -0,0 +1,9 @@
+variable "name" { type = string }
+variable "ami" { type = string }
+variable "instance_type" { type = string }
+variable "subnet_id" { type = string }
+variable "security_group_ids" { type = list(string) }
+variable "key_name" { type = string }
+variable "master_private_ips" { type = list(string) }
+variable "cluster_name" { type = string }
+variable "iam_instance_profile" { type = string }
