6f059374fc21f0436f96e51ca0cca58a32f76976
Juhi Sinha <46685880+juhisinha422@users.noreply.github.com>
2026-01-16 20:36:13 +0530

    Create cdn.tf

---
+++ Notes/CKA/Piyush Notes/Kubernetes_DevOps/Day-100/code/cdn.tf
@@ -0,0 +1,42 @@
+resource "aws_cloudfront_distribution" "cdn" {
+  enabled             = true
+  default_root_object = "index.html"
+
+  aliases = [var.domain_name]
+
+  origin {
+    domain_name              = aws_s3_bucket.site.bucket_regional_domain_name
+    origin_id                = "s3-origin"
+    origin_access_control_id = aws_cloudfront_origin_access_control.oac.id
+  }
+
+  default_cache_behavior {
+    target_origin_id       = "s3-origin"
+    viewer_protocol_policy = "redirect-to-https"
+
+    allowed_methods = ["GET", "HEAD"]
+    cached_methods  = ["GET", "HEAD"]
+
+    compress = true
+
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
+    }
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    acm_certificate_arn = data.aws_acm_certificate.cert.arn
+    ssl_support_method  = "sni-only"
+  }
+
+  price_class = "PriceClass_100"
+}
